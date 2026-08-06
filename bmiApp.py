cb104dbd9fb2712ba4b216b9a317ae4a8ba07ba8
413-26-sithawit <54770@yupparaj.ac.th>
2026-08-06 11:02:51 +0700

    bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -9,7 +9,7 @@ height_cm = st.number_input("กรอกส่วนสูงของคุณ
 if st.button("คํานวณค่า BMI"):
     #แปลงส่วนสูงจาก cm เป็น เมตร แล้วคํานวณ BMI
     height_m = height_cm / 100
-    bmi = weight / (hight_m ** 2)
+    bmi = weight / (height_m ** 2)
 
     st.write("---")
     st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
