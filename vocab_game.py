754f365b52033a6b8ea5a57c95d1990f45db24e4
413-26-sithawit <54770@yupparaj.ac.th>
2026-08-22 15:22:17 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -24,12 +24,13 @@ def reset_game():
 # 📌 ฟังก์ชัน MessageBox (Dialog)
 # ----------------------------------------------------
 @st.dialog("📊 สรุปผลการเล่นเกม")
-def show_result_dialog(ans1, ans2):
+def show_result_dialog(ans1, ans2. ans3):
     st.balloons()
     score = 0
 
     u_ans1 = ans1.strip().lower()
     u_ans2 = ans2.strip().lower()
+    u_ans3 = ans3.strip().lower()
 
     # ตรวจข้อ 1
     if u_ans1 == "apple":
@@ -109,7 +110,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2)
+    show_result_dialog(ans1, ans2, ans3)
 
 st.divider()
 st.write("สิธวิชฎ์ ใจหาญ เลขที่ 26 ม.4/13")
