c1774deea3a514b1bfe6b9304cb3f171d4d595e6
413-26-sithawit <54770@yupparaj.ac.th>
2026-08-22 15:19:48 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -47,10 +47,10 @@ def show_result_dialog(ans1, ans2):
 
     # ตรวจข้อ 3
     if u_ans3 == "fuck":
-        st.success("✅ ข้อ 2: ถูกต้อง")
+        st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
-        st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
+        st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
     if score == 3:
